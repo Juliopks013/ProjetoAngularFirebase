91b312a2f8931efca9bedb788e5d4739db9ea064
DiogoPCS <diogo.paulino.santos@Hotmail.com>
2025-11-14 18:27:32 -0300

    teste

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,25 +4,28 @@ import { CrudService } from '../services/crud.service';
 import { Storage, getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
 import { MessageService } from '../services/message.service';
 import { Router } from '@angular/router';
+import { APIService } from '../services/api.services';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  {
 
-  pokemon:any = {
-    nome: null,
-    poder: null
-  };
+  articles: any;
 
-  constructor( 
-    public crudService: CrudService
-  ){ }
+  constructor(
+    public apiService: APIService
+  ){
+    this.carregarNoticias();
+   }
 
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons');
+  carregarNoticias(){
+    this.apiService.get('top-headlines?country=us&apiKey=20f59a56e8e3454296ace63f237f9089').subscribe((noticias)=>{
+      this.articles = noticias.articles;
+      console.log(this.articles);
+    });
   }
 
 }
